f9dfb1c1439718a3b1c726da4973f8fd1dda660c
ben-han-cn <ben.han.cn@gmail.com>
2018-11-29 09:04:21 +0000

    add scheme

--- example/podwatcher.go
+++ example/podwatcher.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/scheme"
+	"log"
 
 	"github.com/cloudlinker/kubecarve/cache"
 	"github.com/cloudlinker/kubecarve/client/config"
 	"github.com/cloudlinker/kubecarve/controller"
 	"github.com/cloudlinker/kubecarve/event"
 	"github.com/cloudlinker/kubecarve/handler"
+	"github.com/cloudlinker/kubecarve/predicate"
 )
 
 type dumbEventHandler struct {
@@ -19,19 +21,19 @@ type dumbEventHandler struct {
 }
 
 func (d *dumbEventHandler) OnCreate(e event.CreateEvent) (handler.Result, error) {
-	fmt.Printf("---> create kind [%v] with name [%s]\n", e.Object.GetObjectKind(), e.Meta.GetName())
+	log.Printf("create kind [%v] with name [%s]\n", e.Object.GetObjectKind(), e.Meta.GetName())
 	d.podCreateEvent += 1
 	return handler.Result{}, nil
 }
 
 func (d *dumbEventHandler) OnUpdate(e event.UpdateEvent) (handler.Result, error) {
-	fmt.Printf("---> update kind [%v] with name [%s]\n", e.ObjectOld.GetObjectKind(), e.MetaOld.GetName())
+	log.Printf("update kind [%v] with name [%s]\n", e.ObjectOld.GetObjectKind(), e.MetaOld.GetName())
 	d.podUpdateEventCount += 1
 	return handler.Result{}, nil
 }
 
 func (d *dumbEventHandler) OnDelete(e event.DeleteEvent) (handler.Result, error) {
-	fmt.Printf("---> delete kind [%v] with name [%s]\n", e.Object.GetObjectKind(), e.Meta.GetName())
+	log.Printf("delete kind [%v] with name [%s]\n", e.Object.GetObjectKind(), e.Meta.GetName())
 	d.podDeleteEventCount += 1
 	return handler.Result{}, nil
 }
@@ -43,8 +45,7 @@ func (d *dumbEventHandler) OnGeneric(e event.GenericEvent) (handler.Result, erro
 func main() {
 	cfg, err := config.GetConfig()
 	if err != nil {
-		fmt.Printf("---> get config failed %v\n", err)
-		return
+		log.Panic(fmt.Sprintf("get config failed:%v\n", err))
 	}
 
 	stop := make(chan struct{})
@@ -52,16 +53,14 @@ func main() {
 
 	c, err := cache.New(cfg, cache.Options{})
 	if err != nil {
-		fmt.Printf("---> create cache failed %v\n", err)
-		return
+		log.Panic(fmt.Sprintf("create cache failed %v\n", err))
 	}
 	go c.Start(stop)
 
 	c.WaitForCacheSync(stop)
-	fmt.Printf("---> cache synced\n")
 
 	ctrl := controller.New("dumbController", c, scheme.Scheme)
 	ctrl.Watch(&corev1.Pod{})
 	handler := &dumbEventHandler{}
-	ctrl.Start(handler, stop)
+	ctrl.Start(stop, handler, predicate.NewIgnoreUnchangedUpdate())
 }
